d28de4f1251739dc7d1940ad17d98e73492b7c8e
Nurlytan <nurik.bro.2000@mail.ru>
2025-02-28 17:00:05 +0500

    create tables and foreign keys

--- database/migrations/2025_02_28_114834_create_options_table.php
+++ database/migrations/2025_02_28_114834_create_options_table.php
@@ -13,7 +13,12 @@ return new class extends Migration
     {
         Schema::create('options', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger("question_id");
+            $table->text("option");
+            $table->boolean("is_custom");
             $table->timestamps();
+
+            $table->foreign("question_id")->references("id")->on("questions");
         });
     }
 
--- database/migrations/2025_02_28_114839_create_responses_table.php
+++ database/migrations/2025_02_28_114839_create_responses_table.php
@@ -13,7 +13,15 @@ return new class extends Migration
     {
         Schema::create('responses', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger("question_id");
+            $table->unsignedBigInteger("option_id");
+            $table->text("custom_answer");
+            $table->unsignedBigInteger("user_id");
             $table->timestamps();
+
+            $table->foreign("question_id")->references("id")->on("questions");
+            $table->foreign("option_id")->references("id")->on("options");
+            $table->foreign("user_id")->references("id")->on("users");
         });
     }
 
